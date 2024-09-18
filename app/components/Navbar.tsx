26513888bc483bd57a3815f9988a7a26e866ade0
Livingston David Benjamin <livingston@Livingstons-MacBook-Pro.local>
2024-09-18 13:42:06 +0530

    Intialsing repo

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <ul className="flex gap-8 py-4 px-8 w-fit rounded-xl bg-slate-950 text-slate-100 mb-4">
+        <li>Home</li>
+        <li>Focus</li>
+        <li>Work</li>
+    </ul>
+  )
+}
+export default Navbar
